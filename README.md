fe0988d68b0d5876f7dea8a89b9f607b927a1999
jlopez <jimmy.lopez0023@gmail.com>
2020-11-28 10:21:57 +0100

    Code: first version

---
+++ README.md
@@ -0,0 +1,67 @@
+# Random French User Generator
+
+## Version
+
+v0.1
+
+## Install
+
+```
+sudo pip install flask werkzeug.exceptions yaml numpy
+```
+
+## Run
+
+```
+export FLASK_APP=main.py
+export FLASK_ENV=development
+flask run
+```
+
+## Usage
+
+1. With curl:<br/>
+    ```curl -i http://localhost:5000/api/users?n=10```
+
+
+## Parameters
+
+1. **n** (int) : The number of users generate.
+
+2. **gender** (string) : If you want specific gender (other gender are accepted).
+<br/> If not use the result it's a random choice beetween "homme" and "femme". 
+
+3. **firstname** (string): If you want specific firstname.
+
+4. **lastname** (string): If you want specific lastname.
+
+5. **age** (int) : The age of users. We accept multiple value.
+
+6. **age[]** (array) : The age range for users withmin & max value in array.
+
+
+## Output exemple
+
+```
+[
+  {
+    "age": 30, 
+    "firstName": "Emmy", 
+    "gender": "femme", 
+    "id": 0, 
+    "lastName": "Michel", 
+    "uuid": "f9f3e42c-a879-4f99-9cbb-04f75d48dfa8"
+  }
+]
+```
+
+## Other routes
+
+1. All male first name: <br/>
+    ```/api/male/firstname```
+
+2. All female first name: <br/>
+    ```/api/female/firstname```
+
+3. All female first name: <br/>
+    ```/api/lastname```
---
+++ main.py
@@ -0,0 +1,168 @@
+from flask import Flask, request, jsonify, json
+from werkzeug.exceptions import HTTPException
+import random
+import uuid
+import yaml
+import os.path
+import numpy
+
+app = Flask(__name__)
+
+DEBUG = True
+MAX_USERS = 500
+AGE_MIN = 0
+AGE_MAX = 99
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+femaleFirstNamePath = os.path.join(basedir, 'data/female_first_name.yaml')
+maleFirstNamePath = os.path.join(basedir, 'data/male_first_name.yaml')
+lastNamePath = os.path.join(basedir, 'data/last_name.yaml')
+
+def printd(message):
+    if(DEBUG):
+        print(message)
+
+def loadName(path):
+    results = []
+    if os.path.exists(path) and os.path.isfile(path):
+        with open(path, 'r') as yaml_stream:
+            data = yaml.load(yaml_stream, Loader=yaml.SafeLoader)['name']
+            return sorted(data, key=lambda k: random.random())
+
+def getGender(seed, args):
+
+    if 'gender' in args and args["gender"] != "":
+        if args["gender"] == "homme":
+            return "homme"
+        elif args["gender"] == "femme":
+            return "femme"
+        else:
+            return args["gender"]
+
+    return "homme" if seed % 2 else "femme"
+
+def getName(seed, key, names, args):
+    size = len(names)
+
+    if key in args and args[key] != "":
+        return args[key]    
+
+    if size > 0:
+        index = seed % size
+        return names[index]
+    
+    return "XXX"
+
+def getAge(seed, args):
+
+    if "age" in args and args["age"] != "":
+
+        ages = args.getlist("age")
+
+        if len(ages) > 0:
+            index = seed % len(ages)
+            age = ages[index]
+            if age.isdigit():
+                return int(age)
+    
+    elif "age[]" in args and args["age[]"] != "":
+        ages = args.getlist("age[]")
+
+        if len(ages) > 0:
+
+            ags = []
+            for age in ages:
+                if age.isdigit():
+                    ags.append(int(age))
+
+            return random.randrange(min(ags), max(ags))
+            
+    else:
+        return random.randrange(AGE_MIN, AGE_MAX)
+
+    return 0
+
+
+def generateUser(seed, id, femaleFirstName, maleFirstName, lastnames, args):
+
+    firstnames = []
+    gender = getGender(seed, args)
+
+    if gender == "femme":
+        firstnames = femaleFirstName
+    elif gender == "homme":
+        firstnames = maleFirstName
+    else:
+        firstnames = numpy.append(firstnames, femaleFirstName)
+        firstnames = numpy.append(firstnames, maleFirstName)
+
+    result = {
+        "id": id,
+        "uuid": uuid.uuid4(),
+        "gender": gender,
+        "firstName": getName(seed, "firstname", firstnames, args),
+        "lastName": getName(seed, "lastname", lastnames, args),
+        "age": getAge(seed, args)
+    }
+    
+    return result
+
+@app.route('/api/male/firstname', methods=['GET'])
+def allMaleFirstName():
+    content = request.json
+    return jsonify(loadName(maleFirstNamePath))
+
+@app.route('/api/female/firstname', methods=['GET'])
+def allFemaleFirstName():
+    content = request.json
+    return jsonify(loadName(femaleFirstNamePath))
+
+@app.route('/api/lastname', methods=['GET'])
+def allLastName():
+    content = request.json
+    return jsonify(loadName(lastNamePath))
+
+@app.route('/api/users', methods=['GET'])
+def allUsers():
+    content = request.json
+
+    args = request.args
+    printd(args)
+
+    femaleFirstName = loadName(femaleFirstNamePath)
+    maleFirstName = loadName(maleFirstNamePath)
+    lastName = loadName(lastNamePath)
+    
+    n = 0
+    if 'n' in args and args["n"].isdigit():
+        n = int(args["n"])
+
+    if n > MAX_USERS:
+        n = MAX_USERS
+
+    users = []
+    for i in range(0, n):
+        seed = random.randrange(100000, 1000000)
+        users.append(generateUser(seed, i, femaleFirstName, maleFirstName, lastName, args))
+
+    return jsonify(users)
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    response = e.get_response()
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
+
+    
+@app.route('/') 
+def home():
+    return "<h1>Random French User Generator</h1>"
+
+if __name__ == '__main__':
+    app.run(host= '0.0.0.0',debug=DEBUG)
+
